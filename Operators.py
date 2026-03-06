bb0bb636667eecff543591211452d09b729599f1
halitov-r-k <halitov.r.k@gmai.com>
2026-03-06 07:19:21 +0500

    Add Operators.py

---
+++ Operators.py
@@ -0,0 +1,18 @@
+#Operatots
+print("Операторы")
+Num1 = 5
+Num2 = 10
+# +
+print(f"{Num1} + {Num2} =", Num1 + Num2)
+# -
+print(f"{Num2} - {Num1} =", Num2 - Num1)
+# *
+print(f"{Num1} * {Num2} =", Num1 * Num2)
+# /
+print(f"{Num2} / {Num1} =", Num2 / Num1)
+# %
+print(f"{Num2} % {Num1} =", Num2 % Num1)
+# **
+print(f"{Num2} ** {Num1} =", Num2 ** Num1)
+# //
+print(f"{Num2} // {Num1} =", Num2 // Num1)
