d98b2a68d6c7acacef42b3a5e7b7ec42ba77af51
halitov-r-k <halitov.r.k@gmai.com>
2026-03-08 11:30:48 +0500

    2026-03-08T11:30 Add operators

--- Operators.py
+++ Operators.py
@@ -1,18 +1,70 @@
 #Operatots
 print("Операторы")
+# Присваивание =
 Num1 = 5
 Num2 = 10
 # +
-print(f"{Num1} + {Num2} =", Num1 + Num2)
+#print(f"{Num1} + {Num2} =", Num1 + Num2)
 # -
-print(f"{Num2} - {Num1} =", Num2 - Num1)
+#print(f"{Num2} - {Num1} =", Num2 - Num1)
 # *
-print(f"{Num1} * {Num2} =", Num1 * Num2)
+#print(f"{Num1} * {Num2} =", Num1 * Num2)
 # /
-print(f"{Num2} / {Num1} =", Num2 / Num1)
+#print(f"{Num2} / {Num1} =", Num2 / Num1)
 # %
-print(f"{Num2} % {Num1} =", Num2 % Num1)
+#print(f"{Num2} % {Num1} =", Num2 % Num1)
 # **
-print(f"{Num2} ** {Num1} =", Num2 ** Num1)
+#print(f"{Num2} ** {Num1} =", Num2 ** Num1)
 # //
-print(f"{Num2} // {Num1} =", Num2 // Num1)
+#print(f"{Num2} // {Num1} =", Num2 // Num1)
+# +=
+"""res = Num1
+res += Num2
+print(f"Num1 += Num2:",res)"""
+# -=
+'''res = Num2
+res -= Num1
+print(f"Num2 -= Num1:",res)'''
+#*=
+'''res = Num1
+res *= Num2
+print(f"Num1 *= Num2:",res)'''
+#/=
+'''res = Num2
+res /= Num1
+print(f"Num2 /= Num1:",res)'''
+# %= остаток
+'''res = Num2
+res %= Num1
+print(f"Num2 %= Num1:",res)'''
+# //=
+'''res = Num2
+res //= Num1
+print(f"Num2 //= Num1:",res)'''
+# **=
+'''res = Num1
+res **= Num2
+print(f"Num1 **= Num2:",res)'''
+#&=
+'''res = Num2
+res &= Num1
+print(f"Num2 &= Num1:",res)'''
+# |=
+'''res = Num2
+res |= Num1
+print(f"Num2 |= Num1:",res)'''
+# ^=
+'''res = Num1
+res ^= Num2
+print(f"Num1 ^= Num2:",res)'''
+# >>=
+'''res = Num1
+res >>= 1
+print(f"Num1 ^= 1:",res)'''
+# <<=
+'''res = Num1
+res <<= 2
+print(f"Num1 <<= 2:",res)'''
+# :=
+res = Num1
+print(Num1, Num1 := 2, Num1)
