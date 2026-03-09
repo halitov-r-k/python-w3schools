9a91015eaca1741167b1585f9a6b8f24638ef325
halitov-r-k <halitov.r.k@gmai.com>
2026-03-09 09:35:51 +0500

    2026-03-09T9:30 Add operators

--- Operators.py
+++ Operators.py
@@ -65,6 +65,46 @@ print(f"Num1 ^= 1:",res)'''
 '''res = Num1
 res <<= 2
 print(f"Num1 <<= 2:",res)'''
-# :=
-res = Num1
-print(Num1, Num1 := 2, Num1)
+# := Присваивает значени в операторе
+'''res = Num1
+print(Num1, Num1 := 2, Num1)'''
+# comparison
+# value1 == value2 : equal сравнивает значения объектов
+# != not equal
+# > Greater then
+# < Less then
+# >= Greater then or equal to
+# <= Less then or equal to
+# Chaining val1 < val2 < val3
+# Logical
+# val1 and val2
+# or val1 or val2
+# not val1 nor val2
+# Identity
+# value1 is value2 true если один объект.
+# value1 is not value2
+# Membership вхождение в коллекцию
+# value1 in ListValues1
+# value1 not in ListValues1
+# Bitwise сравнение двоичных цифр
+# & - and
+# | - or
+# ^ - xor
+# ~ - not
+# << отбрасывание левых битов, добавление нулей справа
+# >> отбрасывание правых битов добавление копий левого бита
+# Precedence Order Приоритет
+# ()
+# **
+# +x -x ~ x
+# * / // %
+# + -
+# << >>
+# &
+# ^
+# |
+# ==, !=, >, >=, <, <=, is, is not, in, not in
+# not
+# and
+# or
+
