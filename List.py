cac604a6d6395cc005273776679923d74d190f4b
halitov-r-k <halitov.r.k@gmai.com>
2026-03-10 22:09:33 +0500

    2026-03-10T22:10 Add operators

---
+++ List.py
@@ -0,0 +1,21 @@
+#List - набор элементов в одной переменной упорядоченый, допускаются дубликаты индексы с 0
+print("List")
+list1 = [1, 2, 3]
+print("Список list1:", list1)
+# Новые элементы добавляются в конец
+# List можно изменять после создания
+# Длина List len(list)
+print("Длина списка list1:", len(list1))
+# Элементы списка могут быть любого типа. В одном списке могут быть элементы разного типа.
+# Определение типа переменной type(list1)
+print("Тип списка list1", type(list1))
+# Конструктор
+list2 = list([1, 2, 3])
+list3 = list((1, 2, 3))
+print("Список list2:", list2)
+print("Список list3:", list3)
+'''
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members.'''
