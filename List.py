b1a201de063fe834e078cc68b9346a90ee56ffa7
halitov-r-k <halitov.r.k@gmai.com>
2026-03-11 07:07:24 +0500

    2026-03-11T07:10 Add List range

--- List.py
+++ List.py
@@ -1,6 +1,6 @@
 #List - набор элементов в одной переменной упорядоченый, допускаются дубликаты индексы с 0
 print("List")
-list1 = [1, 2, 3]
+list1 = [1, 2, 3, 4, 5]
 print("Список list1:", list1)
 # Новые элементы добавляются в конец
 # List можно изменять после создания
@@ -19,3 +19,15 @@ List is a collection which is ordered and changeable. Allows duplicate members.
 Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
 Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
 Dictionary is a collection which is ordered** and changeable. No duplicate members.'''
+# при отрицательной индексации отсчет начинается с конца
+print("индекс отрицательный -1 в list1", list1[-1])
+# Диапазон индексов начинается с первого значения включаемого до последнего не включаемого
+print("Диапазон индексов list1:", list1[1:3])
+# Если первое значение не указать, то начнется с начала
+print("Диапазон list1 без указания начального элемента:", list1[:3])
+# Если не указать последний элемент, то кончиться последним
+print("Диапазон list1 без указания последнего значения", list1[1:])
+# При указании отрицательных индексов
+print("Вывод list1 с отрицательными индексами в диапазоне:", list1[-4:-1])
+#Проверка существования элемента в List
+print("Проверка существования 3 в list1:", 3 in list1)
