a542b1596d21ad72ca215e7c2a3508b128ca7270
halitov-r-k <halitov.r.k@gmail.com>
2026-04-08 06:54:13 +0500

    set remove, discard, pop, clear, del

--- Sets.py
+++ Sets.py
@@ -15,7 +15,7 @@ set1 = {"value1", "value2", "value3"}
 print(type(set1) == set)'''
 # Constructor set((listVar))
 set2 = set(("value4", "value5", "value6"))
-'''print("set2:", set2)'''
+# print("set2:", set2)
 # доступ к items  по индексу, ключу - не возможен
 # Перебор в цикле for
 '''for item in set1:
@@ -24,10 +24,29 @@ set2 = set(("value4", "value5", "value6"))
 '''print("value1 in set1:", "value1" in set1)
 print("value4 not in set1:", "value4" not in set1)'''
 # Добавление item в set
-'''print("set2:", set2)'''
-set2.add("value7")
-'''print("set2:", set2)'''
+# print("set2:", set2)
+#set2.add("value7")
+# print("set2:", set2)
 # добавление items из набора. Набором может быть list, tuple, dictonaries
-print("set2:", set2)
-set2.update(set1)
-print("set2:", set2)
+# print("set2:", set2)
+# set2.update(set1)
+# print("set2:", set2)
+# удаление items из set
+# print("set2:", set2)
+# set2.remove("value7") # если item не существует - будет сообщение об ошибке
+# set2.discard("value7")# если item не существует - будет сообщение об ошибке не будет
+# print("set2:", set2)
+# Удаление случайного item из set
+# print("set2:", set2)
+# print("удаленный item:", set2.pop())
+# print("set2:", set2)
+# Очистка set
+'''' print("set2:", set2)
+print("Очистка set2")
+set2.clear()
+print("set2:", set2)'''
+# Удаление set
+'''print("set2:", set2)
+print("удаление set2")
+del set2
+print("set2:", set2)''' 
