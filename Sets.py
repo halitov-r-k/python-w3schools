5d2aa8bf7069a6557f4ed53878aaf0335d32a4df
halitov-r-k <halitov.r.k@gmail.com>
2026-04-21 06:55:11 +0500

    frozenset subset, superset

--- Sets.py
+++ Sets.py
@@ -81,8 +81,8 @@ print("set8 = set1.symmetric_difference_update(set2):", set8)# изменяет 
 frozenset1 = frozenset({"value11", "value12", "value13"})
 print("frozenset1:", frozenset1, "type(frozenset1):", type(frozenset1))
 #frozenset.methods
-'''frozenset2 = frozenset1.copy()
-print("frozenset2:", frozenset2)'''
+frozenset2 = frozenset1.copy()
+print("frozenset2:", frozenset2)
 frozenset3 = ({"value12", "value13", "value14"})
 print("frozenset3:", frozenset3)
 '''print("frozenset1.difference(frozenset2):", frozenset1.difference(frozenset3))
@@ -90,7 +90,17 @@ print("frozenset1 - frozenset3:", frozenset1 - frozenset3)'''
 '''print("frozenset1.intersection(frozenset3):", frozenset1.intersection(frozenset3))
 print("frozenset1 & frozenset3:", frozenset1 & frozenset3)'''
 #isdisjoint() непересекаются
-print("fozenset1, frozenset3 не пересекаются:", frozenset1.isdisjoint(frozenset3))
+'''print("fozenset1, frozenset3 не пересекаются:", frozenset1.isdisjoint(frozenset3))'''
 frozenset4 = ({"value15", "value16", "value17"})
 print("frosenset4:", frozenset4)
-print("fozenset1, frozenset4 не пересекаются:",frozenset1.isdisjoint(frozenset4))
+'''print("fozenset1, frozenset4 не пересекаются:",frozenset1.isdisjoint(frozenset4))'''
+# issubsetподмножество 
+frozenset5 ={"value11", "value12", "value13", "value14", "value15", "value16", "value17"}
+print("fozenset5:", frozenset5)
+'''print("frozenset1.issubset(frozenset5:)", frozenset1.issubset(frozenset5))
+print("frozenset1 <= frozenset5:)", frozenset1 <= frozenset5)
+print("frozenset1 < frozenset5:)", frozenset1 < frozenset5)'''
+# issuperset()
+print("frozenset5.issuperset(frozenset1:)", frozenset5.issuperset(frozenset1))
+print("frozenset1 => frozenset5:)", frozenset1 <= frozenset5)
+print("frozenset1 > frozenset5:)", frozenset1 < frozenset5)
